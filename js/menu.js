6512ccbf75ddb7c23d8c35ce19c5b75b4c858846
Slava Petrushin <slava91petrushin@yandex.ru>
2018-05-14 23:42:34 +0300

    menu + js

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,24 @@
 var dropdown__menu = document.querySelector(".dropdown__menu");
-dropdown__menu.onclick = menuShow;//нажатие гамбургера
-
 var close = document.querySelector(".close");
+var btn = document.querySelector(".header__btn");
+
+dropdown__menu.onclick = menuShow;//нажатие гамбургера
 close.onclick = menuHide;//закрытие меню
 
 function menuShow(){
-	console.log(close);
 	var header__menu = document.querySelector(".header__menu");
 	header__menu.style.transform = 'translateX(0%)';
 	
 	close.style.display = 'block';
+	dropdown__menu.style.display = 'none';
+	btn.style.display = 'none';
 }
 
 function menuHide(){
 	document.querySelector('.header__menu').style.transform = 'translateX(-100%)';
-	
 	document.querySelector('.close').style.display = 'none';
+	document.querySelector('.header__btn').style.display = 'block';
+	document.querySelector('.dropdown__menu').style.display = 'block';
 }
 
 
