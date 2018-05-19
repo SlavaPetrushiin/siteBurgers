5b2b342c6216b6b03bf55ec874f877a56f9f4f53
Slava Petrushin <slava91petrushin@yandex.ru>
2018-05-20 01:02:57 +0300

    map

--- js/menu.js
+++ js/menu.js
@@ -29,6 +29,80 @@ function menuHide(){
 
 
 
+//**************************КАРТА***********************************
+ymaps.ready(init);
+
+var placemarks = [
+	{
+		latitude:55.51369140,
+		longitude:36.98030476,
+		hintContent:'<div class="map__hint">Улица Клубная, дом 32</div>',
+		balloonContent:[
+			'<div class="map__balloon">',
+			'<img class="map__img" src="images/logo.svg alt="Бургеры">',
+			'Сaмые вкусные бургеры у нас',
+			'</div>'
+		]
+	},
+	{
+		latitude:55.55126619,
+		longitude:37.07830025,
+		hintContent:'<div class="map__hint">Улица Комсомольская, дом 5</div>',
+		balloonContent:[
+			'<div class="map__balloon">',
+			'<img class="map__img" src="images/logo.svg alt="Бургеры">',
+			'Сaмые вкусные бургеры у нас',
+			'</div>'
+		]		
+	},
+	{
+		latitude:55.55502368,
+		longitude:36.97230567,
+		hintContent:'<div class="map__hint">Калининец, дом 22</div>', 
+		balloonContent:[
+			'<div class="map__balloon">',
+			'<img class="map__img" src="images/logo.svg alt="Бургеры">',
+			'Сaмые вкусные бургеры у нас',
+			'</div>'
+		]		
+	}
+];
+
+var myMap;
+var geoObjects = [];
+function init(){
+	myMap = new ymaps.Map("map", {
+		center:[55.51, 36.98],
+		zoom: 12,
+		controls:['zoomControl'],//элементы управления
+		behaviors:['drag'],//зум карты
+	});
+	
+	for (var i=0; i<placemarks.length; i++){
+		geoObjects[i] = new ymaps.Placemark([placemarks[i].latitude, placemarks[i].longitude],{
+		hintContent:placemarks[i].hintContent, 
+		balloonContent:placemarks[i].balloonContent.join('')
+	},
+	{
+		iconLayout:'default#image',
+		iconImageHref:'images/map-marker.png',
+		iconImageSize:[46, 57],
+		iconImageOffset:[-23,-57]			
+	});
+		
+	};
+	
+	var cluster = new ymaps.Clusterer({
+		
+	});
+	
+	myMap.geoObjects.add(cluster);
+	//myMap.geoObjects.add(placeSelyatino);
+	cluster.add(geoObjects);
+}
+					   
+
+
 
 
 
