c1c5e86234ca71bebbddebf64cc123e236b84482
Slava Petrushin <slava91petrushin@yandex.ru>
2018-05-19 18:32:03 +0300

    pravka

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,18 @@
 var dropdown__menu = document.querySelector(".dropdown__menu");
 var close = document.querySelector(".close");
 var btn = document.querySelector(".header__btn");
+var header__menu = document.querySelector(".header__menu");
 
-dropdown__menu.onclick = menuShow;//нажатие гамбургера
+dropdown__menu.addEventListener('click', menuShow) ;//нажатие гамбургера
 close.onclick = menuHide;//закрытие меню
-
+header__menu.addEventListener('click', function(e){
+	let target = e.target;
+	if(target.className === "menu__link"){
+		menuHide();
+	}	
+})
 function menuShow(){
-	var header__menu = document.querySelector(".header__menu");
+	
 	header__menu.style.transform = 'translateX(0%)';
 	
 	close.style.display = 'block';
@@ -15,9 +21,9 @@ function menuShow(){
 }
 
 function menuHide(){
-	document.querySelector('.header__menu').style.transform = 'translateX(-100%)';
-	document.querySelector('.close').style.display = 'none';
-	document.querySelector('.header__btn').style.display = 'block';
+	header__menu.style.transform = 'translateX(-100%)';
+	close.style.display = 'none';
+	btn.style.display = 'block';
 	document.querySelector('.dropdown__menu').style.display = 'block';
 }
 
